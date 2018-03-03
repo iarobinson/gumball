4743d5eed121a551f204fce3a473c1b42e3f3ff2
Ian <ian (at) ianrobinson.net>
2018-03-03 10:37:30 -0500

    Format JSON and DOM element to display name and # of gumballs sold in human language

--- mightygumball.js
+++ mightygumball.js
@@ -16,5 +16,16 @@ function updateSales(responseText) {
   // // The below code is what we use to dump raw JSON into the DOM
   // //  It doesn't look great, but it shows us the API is connected properly.
   // salesDiv.innerHTML = responseText;
+  
+  // We parse the JSON from a string into an object
   var sales = JSON.parse(responseText);
+  
+  // Now we move through the object entering parsed information into DOM
+  for (var i = 0; i < sales.length; i += 1) {
+    var singleSale = sales[i];
+    var saleDOMObject = document.createElement("div");
+    saleDOMObject.setAttribute("class", "saleItem");
+    saleDOMObject.innerHTML = sale.name + " sold " + sale.sales + " gumballs";
+    salesDiv.appendChild(div);
+  }
 }
