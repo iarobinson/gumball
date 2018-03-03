88829645bbe7c452a346a03327d03e046c2b5099
Ian <ian (at) ianrobinson.net>
2018-03-03 10:20:55 -0500

    Update mightygumball.js to use the sales.json on the GitHub server

--- mightygumball.js
+++ mightygumball.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-  var url = "http://localhost/sales.json";
+  var url = "https://iarobinson.github.io/gumball/sales.json";
   var request = new XMLHttpRequest();
   request.open("GET", url);
   request.onload = function() {
